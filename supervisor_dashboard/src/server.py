b30c141af1c433443786b25452935167655629b7
Jesse <jesse@houzz.com>
2014-04-27 06:06:32 -0700

    timeout version for server proxy

--- supervisor_dashboard/src/server.py
+++ supervisor_dashboard/src/server.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
-from xmlrpclib import ServerProxy, Fault
+from xmlrpclib import Fault
 from urlparse import urlparse
+from httplib import CannotSendRequest
+import socket
 
 from django.utils.datastructures import SortedDict
+
+from src.timeout_server_proxy import TimeoutServerProxy
 
 class Server(object):
     def __init__(self, connection_string, sid):
         self.name = urlparse(connection_string).hostname
-        self.connection = ServerProxy(connection_string)
+        self.connection = TimeoutServerProxy(connection_string, timeout=2)
         self.status = SortedDict()
         self.sid = sid
 
-    def refresh1(self):
+    def refresh(self):
         try:
+            self.status = SortedDict()
             for process in self.connection.supervisor.getAllProcessInfo():
                 key = "%s:%s" % (process['group'], process['name'])
                 process['id'] = key
@@ -20,10 +25,12 @@ class Server(object):
                 if process['name'] != process['group']:
                     process['human_name'] = "%s:%s" % (process['group'], process['name'])
                 self.status[key] = process
-        except Fault, e:
+        except Fault:
+            return False
+        except socket.timeout:
             return False
 
-    def refresh(self):
+    def refresh1(self):
         self.status = SortedDict(("%s:%s" % (i['group'], i['name']), i) for i in self.connection.supervisor.getAllProcessInfo())
         for key, program in self.status.items():
             program['id'] = key
---
+++ supervisor_dashboard/src/timeout_server_proxy.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import xmlrpclib
+import httplib
+import socket
+
+class TimeoutTransport(xmlrpclib.Transport):
+    def __init__(self, timeout=socket._GLOBAL_DEFAULT_TIMEOUT, *args, **kwargs):
+        xmlrpclib.Transport.__init__(self, *args, **kwargs)
+        self.timeout = timeout
+
+    def make_connection(self, host):
+        #return an existing connection if possible.  This allows
+        #HTTP/1.1 keep-alive.
+        if self._connection and host == self._connection[0]:
+            return self._connection[1]
+
+        # create a HTTP connection object from a host descriptor
+        chost, self._extra_headers, x509 = self.get_host_info(host)
+        #store the host argument along with the connection object
+        self._connection = host, httplib.HTTPConnection(chost, timeout=self.timeout)
+        return self._connection[1]
+
+class TimeoutServerProxy(xmlrpclib.ServerProxy):
+    def __init__(self, uri, timeout=socket._GLOBAL_DEFAULT_TIMEOUT,
+                 *args, **kwargs):
+        kwargs['transport'] = TimeoutTransport(timeout=timeout,
+                                    use_datetime=kwargs.get('use_datetime', 0))
+        xmlrpclib.ServerProxy.__init__(self, uri, *args, **kwargs)
